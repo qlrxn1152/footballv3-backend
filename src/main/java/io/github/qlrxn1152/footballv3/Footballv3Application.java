c2ae96e04a84616b9d0a7d0f637b9532e095cee6
qlrxn1152 <qlrxn1152@naver.com>
2026-07-21 12:48:02 +0900

    chore: initialize FootballV3 project

---
+++ src/main/java/io/github/qlrxn1152/footballv3/Footballv3Application.java
@@ -0,0 +1,13 @@
+package io.github.qlrxn1152.footballv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Footballv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Footballv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/qlrxn1152/footballv3/Footballv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.qlrxn1152.footballv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Footballv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
