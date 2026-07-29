1b33413d41c3492efc036063708beee8117bea4a
qlrxn1152 <qlrxn1152@naver.com>
2026-07-29 17:00:45 +0900

    -

--- src/main/java/io/github/qlrxn1152/footballv3/common/config/SecurityConfig.java
+++ src/main/java/io/github/qlrxn1152/footballv3/common/config/SecurityConfig.java
@@ -18,7 +18,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/error").permitAll()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/error").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/members", "/api/auth/login").permitAll() // 회원가입이랑 로그인 요청은 JWT 토큰이 없어도 됨.
                         .requestMatchers(HttpMethod.GET, "/api/teams", "/api/teams/{teamId}").permitAll() // 팀 목록조회, 팀 상세페이지는 모든유저가 가능
                         .requestMatchers(HttpMethod.GET, "/api/matches/pending", "/api/matches/matched", "/api/matches/completed").permitAll() // 매치들 조회는 누구나 가능
